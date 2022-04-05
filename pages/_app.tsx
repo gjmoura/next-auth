d545f4c9de6f65f2fc5ffdb79cfd7514892677d1
Gustavo Jerônimo <gustavo.jeronimo@hotmail.com>
2022-04-04 22:08:40 -0300

    Finaliza aplicação

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
+import '../styles/global.scss'
 import type { AppProps } from 'next/app'
+import { AuthProvider } from '../contexts/AuthContext'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  )
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,38 @@
-import type { NextPage } from 'next';
+import { GetServerSideProps } from 'next';
+import { FormEvent, useContext, useState } from 'react'
 
-const Home: NextPage = () => {
+import { AuthContext } from '../contexts/AuthContext';
+import styles from '../styles/Home.module.scss'
+import { withSSRGuest } from '../utils/withSSRGuest';
+
+export default function Home() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const { signIn } = useContext(AuthContext)
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    const data = {
+      email,
+      password,
+    }
+
+    await signIn(data);
+  }
+
   return (
-    <>
-      <h1>Hello World!</h1>
-    </>
+    <form onSubmit={handleSubmit} className={styles.container}>
+      <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
+      <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+      <button type="submit">Entrar</button>
+    </form>
   )
 }
 
-export default Home
+export const getServerSideProps: GetServerSideProps = withSSRGuest(async (ctx) => {
+  return {
+    props: {}
+  }
+});
